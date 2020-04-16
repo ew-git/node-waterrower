364076b5910f3eca9c5b226dfbfcd806bd74ee03
Evan Wright <evan.btbucket@gmail.com>
2020-04-15 20:03:38 -0400

    Remove extraneous functions from express server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const debug = true;
-
 var waterrower = require("./Waterrower");
 var http = require('http');
 var url = require('url');
@@ -25,51 +23,3 @@ app.get('/data', function (req, res) {
 })
 app.listen(3000, () => console.log('Express app listening on port 3000!'))
 console.log('Server running at http://127.0.0.1:3000/');
-
-var readWaterrower = function() {
-  console.log();
-  console.log("Stroke Rate ....." + waterrower.readStrokeCount());  // [ - ]
-  console.log("Total Speed ....." + waterrower.readTotalSpeed());   // [cm/s]
-  console.log("Average Speed ..." + waterrower.readAverageSpeed()); // [cm/s]
-  console.log("Distance... ....." + waterrower.readDistance());     // [ m ]
-  console.log("Heart Rate ......" + waterrower.readHeartRate());    // [ bpm ]
-
-}
-
-//readWaterrower();
-
-function serverTest(){
-    console.log("Distance... ....." + waterrower.readDistance());
-    setTimeout(serverTest, 1000);
-}
-
-//serverTest();
-
-/*
-setInterval(function() {console.log("Speed in cm/s",
-Number(waterrower.readAverageSpeed()));},
-  1000);
-*/
-
-// Compute more instantaneous 500m split
-var prevdist = Number(waterrower.readDistance()) + Number(waterrower.readDistanceDec())/100;
-var currdist = Number(waterrower.readDistance()) + Number(waterrower.readDistanceDec())/100;
-const distupdaterate = 5000; // ms updating the distance values in interval
-var instantspeed = (currdist - prevdist)/(distupdaterate/1000); // m/s speed
-var instantsplit = 0; // seconds per 500m; 0 means infinite time
-function updateDist(){
-	prevdist = currdist;
-	currdist = Number(waterrower.readDistance()) + Number(waterrower.readDistanceDec())/100;
-	instantspeed = (currdist - prevdist)/(distupdaterate/1000);
-	if (instantspeed < 1){
-		instantsplit = 0;
-	} else {
-		instantsplit = 500/instantspeed;
-	}
-	if (debug){
-		console.log("Instant split:", Math.round(instantsplit, 2), "s ||",
-			"prevdist - currdist:", Math.round(currdist - prevdist, 2), "m");
-	}
-}
-
-//setInterval(updateDist, distupdaterate);
